09f7061d5e05b21285fd580cc052098e0fe21f57
Nawaf <nawaaf.almansour@gmail.com>
2021-01-08 09:40:24 +0300

    add fun animate <main.js>>

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 400;
+
+let spacePressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gameSpeed = 2;
+
+function animate() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.fillRect(10,10,50,50)
+    requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener('keydown', function (e){
+    console.log(e.code);
+})
