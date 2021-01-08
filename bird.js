5be73befd0254929b2976757a14e00a57433a17f
Nawaf <nawaaf.almansour@gmail.com>
2021-01-08 12:38:21 +0300

    add class bird

---
+++ bird.js
@@ -0,0 +1,28 @@
+class Bird {
+    constructor() {
+        this.x = 150;
+        this.y = 200;
+        this.vy = 0;
+        this.width = 20;
+        this.height = 20;
+        this.weight = 1;
+    }
+    update() {
+        if (this.y > canvas.height - (this.height * 3)) {
+            this.y = canvas.height - (this.height * 3)
+            this.vy = 0;
+        } else {
+            this.vy += this.weight;
+            this.y += this.vy;
+        }
+        if (spacePressed) this.flap();
+    }
+    draw(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    flap(){
+        this.vy -= 2;
+    }
+}
+const bird = new Bird();
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d');
 canvas.width = 600;
@@ -14,11 +12,17 @@ let gameSpeed = 2;
 
 function animate() {
     ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.fillRect(10,10,50,50)
+    // ctx.fillRect(10,10,50,50)
+    bird.update();
+    bird.draw();
     requestAnimationFrame(animate);
 }
 animate();
 
 window.addEventListener('keydown', function (e){
-    console.log(e.code);
-})
+    console.log(e.code)
+    if (e.code === 'Space') spacePressed =true;
+});
+window.addEventListener('keyup', function (e){
+    if (e.code === 'Space') spacePressed =false;
+});
